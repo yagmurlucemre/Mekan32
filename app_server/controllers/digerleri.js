a30c5d2a8ab09b3117ede1cb68870e54ae82305a
yagmurlucemre <yagmurlucemre@gmail.com>
2020-12-01 21:38:47 +0300

    'version2'

--- app_server/controllers/digerleri.js
+++ app_server/controllers/digerleri.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 const hakkinda = function (req, res, next) {
-  res.render('hakkinda', { title: 'Hakkında' });
+  res.render('hakkinda', { title: 'Hakkında','footer': 'Cemre Yağmur Ulağ 2020' });
 };
 
 module.exports={
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,121 +2,116 @@
 
 /* GET home page. */
 
- const anaSayfa=function(req, res, next) {
-  res.render('mekanlar-liste',
-   {
-   	'baslik': 'Anasayfa',
-   	'sayfaBaslik':{
-   		
-   		'siteAd':'Mekan32',
-   		'aciklama':'Isparta civarındaki mekanları keşfedin!'
-   	},
-   	'mekanlar':[
-   	{
-   		'ad':'Starbucks',
-   		'adres':'Centrum Garden AVM',
-   		'puan':5,
-   		'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-   		'mesafe':'10km'
-
-   	},
-   	{
-   		'ad':'Gloria Jeans',
-   		'adres':'Iyas AVM',
-   		'puan':3,
-   		'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-   		'mesafe':'5km'
-
-   	},
-   	{
-   		'ad':'Kahve Dünyası',
-   		'adres':'Iyas AVM',
-   		'puan':2,
-   		'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-   		'mesafe':'7km'
-
-   	},
-   	{
-   		'ad':'Antre Kitchen Gurme',
-   		'adres':'Centrum Garden AVM',
-   		'puan':5,
-   		'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-   		'mesafe':'10km'
-
-   	},
-   	{
-   		'ad':'Osmanlı Kahvecisi',
-   		'adres':'Iyas AVM',
-   		'puan':4,
-   		'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-   		'mesafe':'4km'
-
-   	},
-
-
-   	]
-   }
-   );
-}
-const mekanBilgisi=function(req, res, next) {
-  res.render('mekan-detay',{
-
-  	'baslik':'Mekan Bigisi',
-  	'sayfaBaslik':'Starbucks',
-  	'mekanBilgisi':{
-  		'ad':'Starbucks',
-  		'adres':'Centrum Garden AVM',
-  		'puan':3,
-  		'imkanlar':['Kahve','Pasta','Kek'],
-  		'koordinatlar':{
-  			'enlem':37.781885,
-  			'boylam':30.566034
-  		},
-  		'saatler':[
-  		 {
-  			'gunler':'Pazartesi-Cuma',
-  			'acilis':'07:00',
-  			'kapanis':'23:00',
-  			'kapali':false
-  		 },
-  		 {
-  			'gunler':'Cumartesi',
-  			'acilis':'09:00',
-  			'kapanis':'22:30',
-  			'kapali':false
-  		 },
-  		 {
-  			'gunler':'Pazar',
-  			
-  			'kapali':true
-  		 }
-  		],
-  		'yorumlar':[
-  		 {
-  			'yorumYapan':'Cemre Yağmur Ulağ',
-  			'puan':3,
-  			'tarih':'30 Kasım 2020',
-  			'yorumMetni':'Pastaları bir harika,mutlaka denemelisiniz :)'
-  		 }
-
-  		]
-  	}
-  });
-}
- 
-const yorumEkle=function(req, res, next) {
-  res.render('yorum-ekle', { title: 'Yorum Ekle' });
-}
-
-
-
-
-
-
-module.exports={
-anaSayfa,
-mekanBilgisi,
-yorumEkle
-
-
-}
+const anaSayfa=function(req, res, next) {
+	res.render('mekanlar-liste',
+	 {
+		 'baslik': 'Anasayfa',
+		 'footer': 'Cemre Yağmur Ulağ 2020',
+		 'sayfaBaslik':{
+			 
+			 'siteAd':'Mekan32',
+			 'aciklama':'Isparta civarındaki mekanları keşfedin!'
+		 },
+		 'mekanlar':[
+		 {
+			 'ad':'Starbucks',
+			 'adres':'Centrum Garden AVM',
+			 'puan':5,
+			 'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+			 'mesafe':'10km'
+  
+		 },
+		 {
+			 'ad':'Gloria Jeans',
+			 'adres':'İyaş Park AVM',
+			 'puan':4,
+			 'imkanlar':['Çay','Kekler','Pastalar','Kahve'],
+			 'mesafe':'2km'
+  
+		 },
+		 {
+			 'ad':'Osmanlı Kahvecisi',
+			 'adres':'Bahçelievler',
+			 'puan':4,
+			 'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+			 'mesafe':'2km'
+  
+		 },
+		 {
+			 'ad':'Kahve Dünyası',
+			 'adres':'Iyaş Park',
+			 'puan':2,
+			 'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+			 'mesafe':'3km'
+  
+		 },
+		 {
+			 'ad':'Antre Gurme Kitchen',
+			 'adres':'Iyaş Park AVM',
+			 'puan':5,
+			 'imkanlar':['Dünya Mutfağı','Dünya Kahveleri','Kekler','Pastalar'],
+			 'mesafe':'5km'
+  
+		 },
+  
+  
+		 ]
+	 }
+	 );
+  }
+  const mekanBilgisi=function(req, res, next) {
+	res.render('mekan-detay',{
+  
+		'baslik':'Mekan Bigisi',
+		'footer': 'Cemre Yağmur Ulağ 2020',
+		'sayfaBaslik':'Starbucks',
+		'mekanBilgisi':{
+			'ad':'Starbucks',
+			'adres':'Centrum Garden AVM',
+			'puan':5,
+			'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+			'koordinatlar':{
+				'enlem':37.781885,
+				'boylam':30.566034
+			},
+			'saatler':[
+			 {
+				'gunler':'Pazartesi-Cuma',
+				'acilis':'07:00',
+				'kapanis':'23:00',
+				'kapali':false
+			 },
+			 {
+				'gunler':'Cumartesi',
+				'acilis':'09:00',
+				'kapanis':'22:30',
+				'kapali':false
+			 },
+			 {
+				'gunler':'Pazar',
+				
+				'kapali':true
+			 }
+			],
+			'yorumlar':[
+			 {
+				'yorumYapan':'Cemre Yağmur Ulağ',
+				'puan':5,
+				'tarih':'30 Kasım 2020',
+				'yorumMetni':'Pastaları bir harika, mutlaka denemelisiniz :)'
+			 }
+  
+			]
+		}
+	});
+  }
+   
+  const yorumEkle=function(req, res, next) {
+	res.render('yorum-ekle', { title: 'Yorum Ekle','footer': 'Cemre Yağmur Ulağ', });
+  }
+  
+  module.exports={
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle
+  }
