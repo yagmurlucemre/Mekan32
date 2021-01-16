370349b2ecd8b56464fefae1bbb8ab2e884f909f
yagmurlucemre <yagmurlucemre@gmail.com>
2021-01-16 21:00:19 +0300

    son gönderim

--- app_server/controllers/digerleri.js
+++ app_server/controllers/digerleri.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var router = express.Router();
 
 /* GET home page. */
-const hakkinda = function (req, res, next) {
-  res.render('hakkinda', { title: 'Hakkında','footer': 'Cemre Yağmur Ulağ 2020' });
-};
 
-module.exports={
-  hakkinda
+const hakkinda=function(req, res, next) {
+  res.render('hakkinda', { title: 'Hakkında','footer': 'Mücahit Gökkaya 2020', });
 }
-
-
+module.exports={
+	hakkinda
+}
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,117 +1,169 @@
+var request = require ('postman-request');
+var apiSecenekleri = {
+  sunucu : "https://cemreyagmurulag1611012088.herokuapp.com",
+  apiYolu: '/api/mekanlar/'
+}
+var istekSecenekleri
+var footer="Cemre Yağmur Ulağ 2020"
+var mesafeyiFormatla = function (mesafe) {
+  var yeniMesafe, birim;
+  if(mesafe> 1000) {
+    yeniMesafe=parseFloat(mesafe/1000).toFixed(2);
+    birim='km';
+  } else{
+    yeniMesafe=parseFloat(mesafe).toFixed(1);
+    birim='m';
+  }
+    return yeniMesafe + birim;
+} 
+var anaSayfaOlustur =function(req , res , cevap , mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+    mesaj ="API HATASI:Bişeyler ters gitti";
+    mekanListesi = [];
+  } else {
+    if(!mekanListesi.length){
+      mesaj="Civarda Herhangi Bir Mekan Bulunamadı";
+    }
+  }
+  res.render('mekanlar-liste',
+  {
+    baslik:'MekanBul-Yakınındaki Mekanları Bul',
+    sayfaBaslik:{
+      siteAd:'MekanBul',
+      aciklama:'Yakınınızda Kafeleri, Restorantları Bulun!'
+    },
+    footer:footer,
+    mekanlar:mekanListesi,
+    mesaj:mesaj,
+    cevap:cevap
+  });
+}
+const anaSayfa=function(req,res){
+  var istekSecenekleri=
+  {
+    url : apiSecenekleri.sunucu +apiSecenekleri.apiYolu,
+    method : "GET",
+    json :{},
+    qs : {
+      enlem:req.query.enlem,
+      boylam:req.query.boylam
+    }
+  };
+  request(
+    istekSecenekleri,
+    function(hata,cevap,mekanlar){
+      var i, gelenMekanlar;
+      gelenMekanlar=mekanlar;
+      if(!hata && gelenMekanlar.length){
+        for(i=0;i<gelenMekanlar.length;i++){
+          gelenMekanlar[i].mesafe=
+          mesafeyiFormatla(gelenMekanlar[i].mesafe);
+        }
+      }
+      anaSayfaOlustur(req,res,cevap,gelenMekanlar);
+    });
+}
+var detaySayfasiOlustur = function(req,res,mekanDetaylari){
+  res.render('mekan-detay',
+  {
+    baslik:mekanDetaylari.ad,
+    footer:footer,
+    sayfaBaslik:mekanDetaylari.ad,
+    mekanBilgisi:mekanDetaylari
+  });
+}
+var hataGoster = function(req,res,durum){
+  var baslik,icerik;
+  if(durum==404){
+    baslik="404, Sayfa Bulunamadı!";
+    icerik="Kusura Bakma Sayfayı Bulamadım!";
+  }
+  else{
+    baslik=durum+", Birşeyler Ters Gitti! ";
+    icerik="Ters Giden Bişeyler Var!";
+  }
+  res.status(durum);
+  res.render('error',{
+    baslik:baslik,
+    icerik:icerik
+  });
+}
+var mekanBilgisiGetir=function(req,res,callback){
+  var istekSecenekleri={
+    url : apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid,
+    method:"GET",
+    json:{}
+  };
+  request(
+    istekSecenekleri,
+    function(hata,cevap,mekanDetaylari){
+      var gelenMekan =mekanDetaylari;
+      if(cevap.statusCode==200){
+        gelenMekan.koordinatlar={
+          enlem:mekanDetaylari.koordinatlar[0],
+          boylam:mekanDetaylari.koordinatlar[1]
+        };
+        callback(req,res,gelenMekan);
+      }else{
+        hataGoster(req,res,cevap.statusCode);
+      }
+    }
+    );
+};
 
+const mekanBilgisi = function (req, res, callback) {
+  mekanBilgisiGetir(req, res, function(req, res, cevap) {
+    detaySayfasiOlustur(req, res, cevap);
+ });
+};
+var yorumSayfasiOlustur = function(req, res, mekanBilgisi) {
+  res.render('yorum-ekle', {baslik: mekanBilgisi.ad + 'Mekanına Yorum Ekle', sayfaBaslik:mekanBilgisi.ad + 'Mekanına Yorum Ekle',
+    hata: req.query.hata
+});
+};
+const yorumEkle=function(req,res){
+  mekanBilgisiGetir(req, res, function(req, res, cevap){
+    yorumSayfasiOlustur(req, res, cevap);
+  });
+}
+const yorumumuEkle = function(req, res){
+  var istekSecenekleri, gonderilenYorum,mekanid;
+  mekanid=req.params.mekanid;
+  gonderilenYorum = {
+    yorumYapan: req.body.name,
+    puan: parseInt(req.body.rating, 10),
+    yorumMetni: req.body.review
+  };
+  istekSecenekleri = {
+    url : apiSecenekleri.sunucu+ apiSecenekleri.apiYolu+mekanid+'/yorumlar',
+    method : "POST",
+    json : gonderilenYorum
+  };
+  if (!gonderilenYorum.yorumYapan || !gonderilenYorum.puan || !gonderilenYorum.yorumMetni){
+    res.redirect('/mekan/' + mekanid + '/yorum/yeni?hata=evet');
+  } else {
+    request(
+      istekSecenekleri,
+      function(hata, cevap, body) {
+        if (cevap.statusCode === 201) {
+          res.redirect('/mekan/' + mekanid);
+        }
+        else if (cevap.statusCode === 400 && body.name && body.name ==="ValidationError") {
+          res.redirect('/mekan/' + mekanid + '/yorum/yeni?hata=evet');
+        }
+        else {
+          hataGoster(req,res,cevap.statusCode);
+        }
+      }
+       );
+  }
 
-/* GET home page. */
+};
 
-const anaSayfa=function(req, res, next) {
-	res.render('mekanlar-liste',
-	 {
-		 'baslik': 'Anasayfa',
-		 'footer': 'Cemre Yağmur Ulağ 2020',
-		 'sayfaBaslik':{
-			 
-			 'siteAd':'Mekan32',
-			 'aciklama':'Isparta civarındaki mekanları keşfedin!'
-		 },
-		 'mekanlar':[
-		 {
-			 'ad':'Starbucks',
-			 'adres':'Centrum Garden AVM',
-			 'puan':5,
-			 'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-			 'mesafe':'10km'
-  
-		 },
-		 {
-			 'ad':'Gloria Jeans',
-			 'adres':'İyaş Park AVM',
-			 'puan':4,
-			 'imkanlar':['Çay','Kekler','Pastalar','Kahve'],
-			 'mesafe':'2km'
-  
-		 },
-		 {
-			 'ad':'Osmanlı Kahvecisi',
-			 'adres':'Bahçelievler',
-			 'puan':4,
-			 'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-			 'mesafe':'2km'
-  
-		 },
-		 {
-			 'ad':'Kahve Dünyası',
-			 'adres':'Iyaş Park',
-			 'puan':2,
-			 'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-			 'mesafe':'3km'
-  
-		 },
-		 {
-			 'ad':'Antre Gurme Kitchen',
-			 'adres':'Iyaş Park AVM',
-			 'puan':5,
-			 'imkanlar':['Dünya Mutfağı','Dünya Kahveleri','Kekler','Pastalar'],
-			 'mesafe':'5km'
-  
-		 },
-  
-  
-		 ]
-	 }
-	 );
-  }
-  const mekanBilgisi=function(req, res, next) {
-	res.render('mekan-detay',{
-  
-		'baslik':'Mekan Bigisi',
-		'footer': 'Cemre Yağmur Ulağ 2020',
-		'sayfaBaslik':'Starbucks',
-		'mekanBilgisi':{
-			'ad':'Starbucks',
-			'adres':'Centrum Garden AVM',
-			'puan':5,
-			'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-			'koordinatlar':{
-				'enlem':37.781885,
-				'boylam':30.566034
-			},
-			'saatler':[
-			 {
-				'gunler':'Pazartesi-Cuma',
-				'acilis':'07:00',
-				'kapanis':'23:00',
-				'kapali':false
-			 },
-			 {
-				'gunler':'Cumartesi',
-				'acilis':'09:00',
-				'kapanis':'22:30',
-				'kapali':false
-			 },
-			 {
-				'gunler':'Pazar',
-				
-				'kapali':true
-			 }
-			],
-			'yorumlar':[
-			 {
-				'yorumYapan':'Cemre Yağmur Ulağ',
-				'puan':5,
-				'tarih':'30 Kasım 2020',
-				'yorumMetni':'Pastaları bir harika, mutlaka denemelisiniz :)'
-			 }
-  
-			]
-		}
-	});
-  }
-   
-  const yorumEkle=function(req, res, next) {
-	res.render('yorum-ekle', { title: 'Yorum Ekle','footer': 'Cemre Yağmur Ulağ', });
-  }
-  
-  module.exports={
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle
-  }
+module.exports={
+anaSayfa,
+mekanBilgisi,
+yorumEkle,
+yorumumuEkle
+}
