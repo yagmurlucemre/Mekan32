9a3acd1a4189089b0c0e1eae9cb3201cc8f319b4
yagmurlucemre <yagmurlucemre@gmail.com>
2021-01-17 20:52:01 +0300

    son gonderim

--- app_server/controllers/digerleri.js
+++ app_server/controllers/digerleri.js
@@ -2,7 +2,7 @@
 /* GET home page. */
 
 const hakkinda=function(req, res, next) {
-  res.render('hakkinda', { title: 'Hakkında','footer': 'Mücahit Gökkaya 2020', });
+  res.render('hakkinda', { title: 'Hakkında','footer': 'Cemre Yağmur Ulağ 2020', });
 }
 module.exports={
 	hakkinda
